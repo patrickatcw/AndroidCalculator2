6929dc0b93406eaae35090df6d7de0d8f865964a
patrickatcw <pj8392@yahoo.com>
2018-06-17 08:28:54 -0400

    Initial commit for start of project for full layout calculator example

---
+++ app/src/main/java/com/app/me/androidcalculator2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.app.me.androidcalculator2;
+
+//https://www.androidauthority.com/build-a-calculator-app-721910/
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
